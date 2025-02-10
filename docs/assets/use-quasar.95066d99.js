f4ffeda2e5f9c96d50ed8fa96741a1428f5cf023
Vismyfriend <vismyfriend@yahoo.com>
2025-02-10 09:21:33 +0300

    public path

--- docs/assets/use-quasar.95066d99.js
+++
@@ -1,1 +0,0 @@
-import{C as a,D as r}from"./index.f9dcf5ce.js";function u(){return a(r)}export{u};
--- quasar.config.js
+++ quasar.config.js
@@ -71,7 +71,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: '/docs/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
