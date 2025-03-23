d3ec1c93a76c736972fbd786140bac8457fdfe00
vincentspecial <littlev@ya.ru>
2025-03-23 14:48:34 +0300

    npx quasar build

---
+++ docs/assets/special logo detective girl 2.c2cfe235.js
@@ -0,0 +1,1 @@
+var s="/special/assets/backgroundBlur.638f1f18.jpg",a="/special/assets/special logo detective girl 2.612c8499.png";export{s as _,a};
---
+++ docs/assets/use-quasar.3d06dbe1.js
@@ -0,0 +1,1 @@
+import{j as a,q as r}from"./index.b9d2f74e.js";function u(){return a(r)}export{u};
