edd79431da4023a718b086911085cb232219b88a
Vismyfriend <131457887+vismyfriend@users.noreply.github.com>
2026-05-13 18:44:13 +0300

    v128.71 correctAnswer correctAnswers fixed in game millionaire

--- docs/assets/ThisGameIsComingSoon.4c5da712.js
+++
@@ -1,1 +0,0 @@
-import{p as e,u as o}from"./index.16f473c1.js";import"./short-words-data.d17ad4da.js";const n={__name:"ThisGameIsComingSoon",setup(s){return e(),o(),(t,r)=>null}};export{n as default};
